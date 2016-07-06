5c0db88f3ab3a09928f4d62a74b32d6132226325
Blake Contreras <blakecontreras@users.noreply.github.com>
2016-07-06 14:06:28 -0700

    Create puppy.js

---
+++ puppy.js
@@ -0,0 +1,8 @@
+// This is a test file with some example code. 
+// Change this file any way you want and try to handle conflicts
+// when you pull --rebase
+
+let puppy = "puppy"
+for (var i = 0; i < puppy.length; i++) {
+  console.log(puppy[i]);
+}
